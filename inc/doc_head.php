98bf9057d28862aa45afce7872a7091ee6f85f1d
jjy <damanegi1979@naver.com>
2016-05-25 11:30:11 +0900

    no message

---
+++ inc/doc_head.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+<meta charset="utf-8" />
+<meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<link rel="stylesheet" href="/lib/css/style.css" />
+<!--[if lt IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+<![endif]-->
+<script src="/lib/js/jquery-1.9.1.min.js"></script>
+<script src="/lib/js/ui.js" ></script>
+
+<!-- //favicon, app icon -->
+<link rel="shortcut icon" href="" />
+<link rel="apple-touch-icon" href="" />
+
+<title>ICE FACTORY</title>
+<meta name="description" content=""/>
+<meta name="keywords" content=""/>
+</head>
---
+++ inc/header.php
@@ -0,0 +1,4 @@
+<header>
+    <h1>ICE FACTORY</h1>
+    <p class="account">홍길동님 안녕하세요. <button>로그아웃</button></p>
+</header>
